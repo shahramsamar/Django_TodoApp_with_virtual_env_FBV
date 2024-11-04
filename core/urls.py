d897837c23a174e68b5a614596ee55244a3a072f
shahramsamar <shahramsamar2010@gmail.com>
2024-11-04 23:09:13 +0330

    create app todo

--- core/urls.py
+++ core/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('todo',include('todo.urls')),
+    path('',include('todo.urls')),
 ]
 
 if settings.DEBUG:
